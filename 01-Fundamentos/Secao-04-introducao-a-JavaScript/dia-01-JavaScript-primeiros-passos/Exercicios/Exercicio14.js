0cce724d36f76813e79812955dbaa0a5a3918c2d
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2022-09-22 15:17:58 -0300

    Secao 4 exercicio 13 - 14 e 15

--- 01-Fundamentos/Secao-04-introducao-a-JavaScript/dia-01-JavaScript-primeiros-passos/Exercicios/Exercicio14.js
+++ 01-Fundamentos/Secao-04-introducao-a-JavaScript/dia-01-JavaScript-primeiros-passos/Exercicios/Exercicio14.js
@@ -1,31 +1,16 @@
-// const salarioBruto = 3000;
-// let inss = 0;
-// let ir = 0;
+const custoProduto = 100;
+const valorVenda = 1000;
 
-// // INSS (Instituto Nacional do Seguro Social) 
-// if (salarioBruto <= 1556.94) {
-// inss = 0.08 * salarioBruto;
-// } else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
-// inss = 0.09 * salarioBruto;
-// } else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
-// inss = 0.11 * salarioBruto;
-// } else {
-// inss = 570.88;
-// }
+const imposto = custoProduto * 0.2;
+const custoTotal = imposto + custoProduto;
+const lucro = valorVenda - custoTotal;
 
-// const salarioMenosInss = salarioBruto - inss;
+// Seu programa também deve emitir uma mensagem de erro e encerrar caso algum dos seus valores de entrada seja menor que zero. :white_check_mark:
+if (custoProduto > 0 && valorVenda > 0) {
+console.log('Lucro em um produto: ', lucro);
+console.log('Lucro em 1000 produtos: ', lucro * 1000);
+} else {
+console.log('Valores inválidos');
+} 
 
-// // IR (Imposto de Renda)
-// if (salarioMenosInss <= 1903.98) {
-// ir = 0;
-// } else if (salarioMenosInss >= 1903.99 && salarioMenosInss <= 2826.65) {
-// ir = (salarioMenosInss * 0.075) - 142.80;
-// } else if (salarioMenosInss >= 2826.66 && salarioMenosInss <= 3751.05) {
-// ir = (salarioMenosInss * 0.15) - 354.80;
-// } else if (salarioMenosInss >= 3751.06 && salarioMenosInss <= 4664.68) {
-// ir = (salarioMenosInss * 0.225) - 636.13;
-// } else {
-// ir = (salarioMenosInss * 0.275) - 868.36;
-// }
-
-// console.log('Salário final:', salarioMenosInss - ir); 
+// Exercicio elaborado na mentoria
---
+++ 01-Fundamentos/Secao-04-introducao-a-JavaScript/dia-01-JavaScript-primeiros-passos/Exercicios/Exercicio15.js
@@ -0,0 +1,33 @@
+const salarioBruto = 3000;
+let inss = 0;
+let ir = 0;
+
+// INSS (Instituto Nacional do Seguro Social) 
+if (salarioBruto <= 1556.94) {
+inss = 0.08 * salarioBruto;
+} else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
+inss = 0.09 * salarioBruto;
+} else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
+inss = 0.11 * salarioBruto;
+} else {
+inss = 570.88;
+}
+
+const salarioMenosInss = salarioBruto - inss;
+
+// IR (Imposto de Renda)
+if (salarioMenosInss <= 1903.98) {
+ir = 0;
+} else if (salarioMenosInss >= 1903.99 && salarioMenosInss <= 2826.65) {
+ir = (salarioMenosInss * 0.075) - 142.80;
+} else if (salarioMenosInss >= 2826.66 && salarioMenosInss <= 3751.05) {
+ir = (salarioMenosInss * 0.15) - 354.80;
+} else if (salarioMenosInss >= 3751.06 && salarioMenosInss <= 4664.68) {
+ir = (salarioMenosInss * 0.225) - 636.13;
+} else {
+ir = (salarioMenosInss * 0.275) - 868.36;
+}
+
+console.log('Salário final:', salarioMenosInss - ir); 
+
+// Exercicio elaborado na mentoria
