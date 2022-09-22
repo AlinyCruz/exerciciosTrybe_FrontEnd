7f8854b57faf5d61076719ef45ef87f52dd1f1e9
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2022-09-22 13:56:24 -0300

    atualizando

--- 01-Fundamentos/Secao-04-introducao-a-JavaScript/dia-01-JavaScript-primeiros-passos/Exercicios/Exercicio13.js
+++ 01-Fundamentos/Secao-04-introducao-a-JavaScript/dia-01-JavaScript-primeiros-passos/Exercicios/Exercicio13.js
@@ -1,13 +1,13 @@
-const custoProduto = 100;
-const valorVenda = 1000;
-const imposto = custoProduto * 0.2;
-const custoTotal = imposto + custoProduto;
-const lucro = valorVenda - custoTotal;
+// const custoProduto = 100;
+// const valorVenda = 1000;
+// const imposto = custoProduto * 0.2;
+// const custoTotal = imposto + custoProduto;
+// const lucro = valorVenda - custoTotal;
 
-// Seu programa também deve emitir uma mensagem de erro e encerrar caso algum dos seus valores de entrada seja menor que zero. :white_check_mark:
-if (custoProduto > 0 && valorVenda > 0) {
-console.log('Lucro em um produto: ', lucro);
-console.log('Lucro em 1000 produtos: ', lucro * 1000);
-} else {
-console.log('Valores inválidos');
-} 
+// // Seu programa também deve emitir uma mensagem de erro e encerrar caso algum dos seus valores de entrada seja menor que zero. :white_check_mark:
+// if (custoProduto > 0 && valorVenda > 0) {
+// console.log('Lucro em um produto: ', lucro);
+// console.log('Lucro em 1000 produtos: ', lucro * 1000);
+// } else {
+// console.log('Valores inválidos');
+// } 
