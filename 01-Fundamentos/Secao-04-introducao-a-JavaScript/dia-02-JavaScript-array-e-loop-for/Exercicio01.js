a4fa3969c8da690f5b1da05eb65f121fe174880c
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2022-09-22 15:37:44 -0300

    Secao 4 - Dia 2 - exercicio 1

---
+++ 01-Fundamentos/Secao-04-introducao-a-JavaScript/dia-02-JavaScript-array-e-loop-for/Exercicio01.js
@@ -0,0 +1,20 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+//----------------------------------------------------//
+
+let menu2 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu2[2];
+
+console.log(indexOfPortfolio);
+
+//----------------------------------------------------//
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+menu.push('Contato')
+
+console.log(menu);
+
