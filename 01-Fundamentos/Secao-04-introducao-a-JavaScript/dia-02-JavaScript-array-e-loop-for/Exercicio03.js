48a17c8d08ae5a16984617e6640e7ec74901b26b
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2022-09-22 22:12:45 -0300

    Secao 4 - Dia 2 - exercicio 3 parte

---
+++ 01-Fundamentos/Secao-04-introducao-a-JavaScript/dia-02-JavaScript-array-e-loop-for/Exercicio03.js
@@ -0,0 +1,57 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// // Percorra o array imprimindo todos os valores nele contidos com a função console.log();
+for (let index = 0; index < numbers.length; index += 1) {
+    console.log(numbers[index]);
+}
+
+// Some todos os valores contidos no array e imprima o resultado;
+let soma = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+    soma = soma + numbers[index]; //<--- soma += numbers[index];
+}
+console.log(soma);
+
+// Calcule e imprima a média aritmética dos valores contidos no array;
+// A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.
+let media = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+    media = soma / numbers.length;
+}
+console.log(media);
+
+// Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”;
+if (media > 20) {
+    console.log('Valor maior que 20');
+} else {
+    console.log('Valor menor ou igual a 20');
+}
+
+// Utilizando for, descubra qual o maior valor contido no array e imprima-o;
+let maiorValor = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] > maiorValor) {
+        maiorValor = numbers[index];
+    }
+}
+console.log(`O maior valor é o ${maiorValor}!`);
+
+// Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “nenhum valor ímpar encontrado”;
+let impares = 0;
+let quantidade = [];
+
+for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] % 2 !== 0) {
+        impares = numbers[index];
+        console.log(impares);
+    }
+}
+
+
+
+
+// Utilizando for, descubra qual o menor valor contido no array e imprima-o;
+
+// Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
+
+// Utilizando o array que acabou de criar, imprima o resultado da divisão de cada um dos elementos por 2.
