e0cf9120ae0f60bce34a6ec21950d493a0983f1a
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2022-09-28 18:13:00 -0300

    Secao 5 - Dia 1 - exercicio 1

---
+++ 01-Fundamentos/Secao-05-JavaScript-DOM-Eventos-e-WebStorage/dia-01-JavaScript-DOM-e-seletores/Exercicio01/script.js
@@ -0,0 +1,21 @@
+const paragraph = document.getElementById("paragraph");
+      paragraph.style.color = "red";
+
+const pageTitle = document.getElementById("page-title");
+      pageTitle.innerText = "A lagoa Azul";
+
+const secondParagraph = document.getElementById("second-paragraph");
+      secondParagraph.style.background = "green";
+
+const subtitle = document.getElementById("subtitle");
+      subtitle.style.color = "pink";
+
+const paragrafos = document.getElementsByClassName('paragrafos');
+      paragrafos[0].style.background = "yellow"
+      // for (let i = 0; i < paragrafos.length; i+= 1) {
+      // paragrafos[i].style.background = "yellow";
+      // }
+
+const subtitulo = document.getElementsByTagName('h4');
+      subtitulo[0].innerText = "Esta frase esta na cor rosa"
+
