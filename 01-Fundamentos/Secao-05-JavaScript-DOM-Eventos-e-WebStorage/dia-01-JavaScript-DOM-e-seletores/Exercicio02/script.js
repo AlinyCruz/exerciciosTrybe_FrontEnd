70e04eb3f412bcccf57ca0edeee5695bc243b40f
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2022-09-28 19:39:15 -0300

    Secao 5 - Dia 1 - exercicio 2

---
+++ 01-Fundamentos/Secao-05-JavaScript-DOM-Eventos-e-WebStorage/dia-01-JavaScript-DOM-e-seletores/Exercicio02/script.js
@@ -0,0 +1,27 @@
+const titulo = document.getElementById('header-container');
+    titulo.style.background = 'green';
+
+const body = document.getElementById('container');
+    body.style.background = 'gray';
+
+const section = document.getElementsByClassName('emergency-tasks')[0];
+    section.style.background = 'pink';
+
+const section2 = document.getElementsByClassName('no-emergency-tasks')[0];
+    section2.style.background = 'yellow';
+
+const tituloSection = document.querySelectorAll('.emergency-tasks h3')[0];
+    tituloSection.style.background = 'purple';
+
+const tituloSection2 = document.querySelectorAll('.emergency-tasks h3')[1];
+    tituloSection2.style.background = 'purple';
+
+const tituloSection3 = document.querySelectorAll('.no-emergency-tasks h3')[0];
+    tituloSection3.style.background = 'black';
+
+const tituloSection4 = document.querySelectorAll('.no-emergency-tasks h3')[1];
+    tituloSection4.style.background = 'black';
+
+const rodape = document.getElementById('footer-container');
+    rodape.style.background = 'green';
+
