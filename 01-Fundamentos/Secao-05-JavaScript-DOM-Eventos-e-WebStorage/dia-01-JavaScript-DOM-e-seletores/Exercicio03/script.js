e5237116a4eba7868104f04389b40a15eb08cd8b
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2022-09-29 00:57:23 -0300

    Secao 5 - Dia 1 - exercicio 3

---
+++ 01-Fundamentos/Secao-05-JavaScript-DOM-Eventos-e-WebStorage/dia-01-JavaScript-DOM-e-seletores/Exercicio03/script.js
@@ -0,0 +1,32 @@
+//Aqui você vai modificar os elementos já existentes utilizando apenas as funções:
+
+//  - document.getElementById()
+//  - document.getElementsByClassName()
+//  - document.getElementsByTagName()
+     
+// 1. Crie e execute uma função que mude o texto na tag `<p>-----</p>`, para uma descrição de como você se vê daqui a 2 anos.
+// (Não gaste tempo pensando no texto e sim realizando o exercício)
+const paragrafo2 = document.getElementsByTagName('p')[1];
+paragrafo2.innerText = 'Bem de vida, realizada profissionalmente e financeiramente.'
+      
+// 2. Crie e execute uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
+const mainContent = document.getElementsByClassName('main-content')[0];
+mainContent.style.background = 'rgb(76,164,109)';
+
+// 3. Crie e execute uma função que mude a cor do quadrado vermelho para branco.
+const centerContent = document.getElementsByClassName('center-content')[0];
+centerContent.style.background = 'white';
+
+// 4. Crie e execute uma função que corrija o texto da tag <h1>.
+const titulo = document.getElementsByTagName('h1')[0];
+titulo.innerText = 'Exercício - JavaScript'
+
+// 5. Crie e execute uma função que modifique o texto da primeira tag <p> para maiúsculo.
+const paragrafo1 = document.getElementsByTagName('p')[0];
+paragrafo1.innerText = 'TEXTO PADRÃO DO NOSSO SITE'
+
+// 6. Crie e execute uma função que exiba o conteúdo de todas as tags <p> no console.
+const paragrafos = document.getElementsByTagName('p');
+    for(i = 0; i < paragrafos.length; i += 1) {
+        console.log(paragrafos[i].innerText)
+    }
