ff92ed2065029b373059f7b03333be878bf0e88d
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2022-09-30 15:46:59 -0300

    Secao 5 - Dia 2 - exercicio 1 parte

--- 01-Fundamentos/Secao-05-JavaScript-DOM-Eventos-e-WebStorage/dia-02-JavaScript-Trabalhando-com-elementos/Exercicio01/script.js
+++ 01-Fundamentos/Secao-05-JavaScript-DOM-Eventos-e-WebStorage/dia-02-JavaScript-Trabalhando-com-elementos/Exercicio01/script.js
@@ -1,9 +1,11 @@
+//Exercicios parte 01
+
 //Acesse o elemento elementoOndeVoceEsta.
-document.querySelector('#elementoOndeVoceEsta'); // acessa o 2º filho do pai 
+const ondeVoceEsta = document.querySelector('#elementoOndeVoceEsta'); // acessa o 2º filho do pai 
 
 //Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
 const pai = document.querySelector('#elementoOndeVoceEsta').parentElement; // acessa o pai
-pai.style.color = 'blue';
+pai.style.color = 'magenta';
 pai.style.fontSize = '30px';
 
 //Acesse o primeiroFilhoDoFilho e adicione um texto a ele.
@@ -25,4 +27,33 @@ document.querySelector('#elementoOndeVoceEsta').nextElementSibling;
 
 //Agora acesse o terceiroFilho a partir de pai.
 document.querySelector('#pai').childNodes[5];
+
+//Exercicios parte 02
+
+//Crie um irmão para elementoOndeVoceEsta.
+const newSection = document.createElement('section');
+pai.appendChild(newSection);
+
+newSection.innerText = 'Oi sou seu novo filho';
+
+//Crie um filho para elementoOndeVoceEsta.
+const newDiv = document.createElement('div');
+ondeVoceEsta.appendChild(newDiv);
+
+newDiv.innerText = 'Oi sou sua nova filha';
+
+//Crie um filho para primeiroFilhoDoFilho.
+const primeiroFilhoDoFilho = document.querySelector('#primeiroFilhoDoFilho');
+const newArticle = document.createElement('article');
+primeiroFilhoDoFilho.appendChild(newArticle);
+
+newArticle.innerText = 'Oi sou a sua caçula';
+
+//A partir desse filho criado, acesse terceiroFilho.
+newArticle.setAttribute('id','son');
+document.querySelector('#primeiroFilhoDoFilho');
+
+//Exercicios parte 03
+
+//Remova todos os elementos filhos de paiDoPai exceto pai, elementoOndeVoceEsta e primeiroFilhoDoFilho.
 
