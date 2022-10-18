903d41314340369e1c4351bdc1adeda05af59078
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2022-10-18 14:59:53 -0300

    Secao 7 - Dia 1 - Exercicios parte 01.

---
+++ 01-Fundamentos/Secao-07-Introdução-a-JavaScript-ES6-e-Testes-Unitarios/Dia-01-JavaScript-ES6-let-const-arrow-functions-e-template-literals/Parte02-Exercicio01.js
@@ -0,0 +1,19 @@
+//Crie a função factorial que recebe um número como parâmetro, utilizando arrow functions
+//(Lembre-se de armazenar a função utilizando o tipo de variável correta). Exemplo: 4! = 4 * 3 * 2 * 1 = 24
+
+let factorial = (n) => {
+  //Dentro da função crie uma variável result, que será o retorno final da função.
+  let result = n;
+  //Crie a lógica para retornar o fatorial de N!.
+  for (let i = n - 1; i >= 1; i-= 1) {
+    result = result * i;
+  } 
+  //Imprima no terminal “Esse é o fatorial resultado da função“ (Lembre-se de utilizar template literals nesse momento).
+  console.log(`${result}, esse é o fatorial do resultado da função!`);  
+}
+factorial(4);
+
+//Bônus: tente fazer o mesmo exercício de forma recursiva.
+//Spoiler: É possível resolver com uma linha usando ternary operator.
+
+//??????
---
+++ 01-Fundamentos/Secao-07-Introdução-a-JavaScript-ES6-e-Testes-Unitarios/Dia-01-JavaScript-ES6-let-const-arrow-functions-e-template-literals/Parte02-Exercicio02.js
@@ -0,0 +1,4 @@
+//Crie uma função que receba uma frase como parâmetro e retorne a maior palavra contida nesta frase.
+//Exemplo: longestWord('Antônio foi ao banheiro e não sabemos o que aconteceu') // retorna 'aconteceu'
+
+//Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort().
