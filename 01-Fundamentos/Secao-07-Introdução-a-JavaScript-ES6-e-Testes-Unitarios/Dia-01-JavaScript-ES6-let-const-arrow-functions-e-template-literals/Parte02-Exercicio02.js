d1011b22eacc7c0528db3c8623f2cfd8ca6a52a7
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2022-10-19 11:08:24 -0300

    Secao 7 - Dia 1 - Parte 02 Exercicio 02.

--- 01-Fundamentos/Secao-07-Introdução-a-JavaScript-ES6-e-Testes-Unitarios/Dia-01-JavaScript-ES6-let-const-arrow-functions-e-template-literals/Parte02-Exercicio02.js
+++ 01-Fundamentos/Secao-07-Introdução-a-JavaScript-ES6-e-Testes-Unitarios/Dia-01-JavaScript-ES6-let-const-arrow-functions-e-template-literals/Parte02-Exercicio02.js
@@ -1,4 +1,24 @@
 //Crie uma função que receba uma frase como parâmetro e retorne a maior palavra contida nesta frase.
 //Exemplo: longestWord('Antônio foi ao banheiro e não sabemos o que aconteceu') // retorna 'aconteceu'
 
+const contaPalavra = (param) => {
+  const palavra = param.split(' ');
+  let maiorPalavra = 0;
+  let resultado = ' ';
+  
+  for (const key of palavra) {
+    if (key.length > maiorPalavra) {
+      maiorPalavra = key.length;
+      resultado = key;
+    }
+  }
+  return resultado;
+}
+console.log(contaPalavra('Antônio foi ao banheiro e não sabemos o que aconteceu'));
+
 //Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort().
+// Extarido do gabarito:
+
+  const longestWord = text => text.split(' ').sort((wordA, wordB) => wordB.length - wordA.length)[0];
+
+  console.log(longestWord("Antonio foi ao banheiro e não sabemos o que aconteceu"));
