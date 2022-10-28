a6fb329e52bda211b848eb431360e8eb74dec1dd
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2022-10-28 20:55:21 -0300

    Secao 8 - Dia 05 - Explicacao 03 parte

--- 01-Fundamentos/Secao-08-Higher-Order-Functions-do-JavaScript-ES6/Dia-05-JavaScript-ES6-spread-operator-parametro-rest-destructuring-e-mais/Explicacao03.js
+++ 01-Fundamentos/Secao-08-Higher-Order-Functions-do-JavaScript-ES6/Dia-05-JavaScript-ES6-spread-operator-parametro-rest-destructuring-e-mais/Explicacao03.js
@@ -10,4 +10,15 @@ const [first, second] = saudacoes;
 
 console.log(second(first));
 
+//-----------------------------------------------------------------------------------------------//
 
+// A seguir, temos alguns valores que estão descritos em variáveis incoerentes.
+// Através da desestruturação de arrays, corrija os valores das variáveis.
+
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+console.log(comida, animal, bebida); // arroz gato água
+
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
