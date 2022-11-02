59f901fefd8310b61358c8776ca17b812acc9ada
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2022-11-02 03:31:49 -0300

    Secao 8 - Dia 1 - Exercicio 03

--- 01-Fundamentos/Secao-08-Higher-Order-Functions-do-JavaScript-ES6/Dia-01-JavaScript-ES6-Introducao-a-Higher-Order-Functions/Exercicios/Exercicio03.js
+++ 01-Fundamentos/Secao-08-Higher-Order-Functions-do-JavaScript-ES6/Dia-01-JavaScript-ES6-Introducao-a-Higher-Order-Functions/Exercicios/Exercicio03.js
@@ -3,12 +3,35 @@
 
 // O primeiro será um array de respostas corretas (soluções);
 // O segundo será um array contendo as respostas fornecidas por uma pessoa estudante;
-// O terceiro é uma função que compara os dois arrays e então dá uma pontuação baseada nos acertos. Para isso, essa função usará os seguintes critérios:
+// O terceiro é uma função que compara os dois arrays e então dá uma pontuação baseada nos acertos.
+// Para isso, essa função usará os seguintes critérios:
   // Uma resposta correta adiciona 1 ponto à pontuação final;
   // A ausência de uma resposta não altera a pontuação (quando for “N.A”);
   // Uma resposta incorreta reduz a pontuação final em 0.5 ponto.
 
-// Ao final, a HOF deve retornar o total de pontos obtidos através das respostas fornecidas pela pessoa estudante. Utilize os seguintes arrays:
+// Ao final, a HOF deve retornar o total de pontos obtidos através das respostas fornecidas pela pessoa estudante.
+// Utilize os seguintes arrays:
 
 const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
 const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const verificadorPontos = (array1, array2) => {
+      if (array1 === array2) {
+        return 1;
+      } if (array2 === 'N.A') {
+        return 0;
+      } 
+      return -0.5;
+}
+
+const corretorAutomatico = (solucoes , estudante, funcao) => {
+  let verificador = 0;
+  for (let i = 0; i < solucoes.length; i +=1) {
+    const soma = funcao(solucoes[i], estudante[i]);
+    verificador += soma;
+  }
+  return verificador;
+}
+console.log(corretorAutomatico(RIGHT_ANSWERS, STUDENT_ANSWERS, verificadorPontos));
+
+// Exercicio extraido gabarito
