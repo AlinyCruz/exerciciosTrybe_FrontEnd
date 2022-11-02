b05eac614795fc814687e7ef83ff0bfb36407295
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2022-11-02 16:51:54 -0300

    Secao 8 - Dia 3 - Exercicio 01

---
+++ 01-Fundamentos/Secao-08-Higher-Order-Functions-do-JavaScript-ES6/Dia-03-JavaScript-ES6-Higher-Order-Functions-map-e-filter/Exercicio02.js
@@ -0,0 +1,5 @@
+// Construa um array de objetos a partir do array de livros. 
+// Cada objeto deve conter uma propriedade author, com o nome da pessoa autora do livro, e uma propriedade age, com a idade dessa pessoa quando o livro foi lançado. 
+// O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha, considerando suas idades quando o livro foi lançado.
+// De olho na dica 👀: use as funções map, sort
+
