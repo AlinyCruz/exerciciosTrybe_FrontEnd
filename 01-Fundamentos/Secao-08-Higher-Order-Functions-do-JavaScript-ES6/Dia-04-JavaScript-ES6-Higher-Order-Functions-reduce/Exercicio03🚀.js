57fb77ebec76e861a378f431517c260b29f32186
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2022-11-08 18:53:33 -0300

    Secao 8 - Dia 1 - Exercicio 04

---
+++ 01-Fundamentos/Secao-08-Higher-Order-Functions-do-JavaScript-ES6/Dia-04-JavaScript-ES6-Higher-Order-Functions-reduce/Exercicio03🚀.js
@@ -0,0 +1,19 @@
+// Dado o array de nomes, utilize o reduce para retornar a quantidade de vezes em que aparece a letra a, maiúscula ou minúscula.
+
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+// const expectedResult = 20;
+
+// Step 1 -> Acessar os dados da variável 'names'
+// Step 2 -> Acessar letra por letra
+// Step 3 -> Verificar se cada letra é a 'A'
+// Step 4 -> Incrementar as informaçoes verdadeiras
+// Step 5 -> Retornar o resultado final
+
+function containsA() {
+  // escreva seu código aqui
+}
