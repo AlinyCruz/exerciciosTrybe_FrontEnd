1b7e89cfa9b7d09f4d50386f7a7f9242cc2864c1
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2022-11-22 18:55:57 -0300

    Hello World - React

---
+++ 02-Front-End/Secao-02-Introducao-ao-React/Dia-01-Hello-world-no-React/minha-aplicacao/src/About.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{'Aline'}</h1>
+        <p>{'Eu sou Tryber'}</p>
+        <h2>{'Minhas Habilidades'}</h2>
+        <ul>
+          <li>{'Dialogar'}</li>
+          <li>{'Determinação'}</li>
+          <li>{'Responsabilidade'}</li>
+          <li>{'Empatia'}</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default About;
