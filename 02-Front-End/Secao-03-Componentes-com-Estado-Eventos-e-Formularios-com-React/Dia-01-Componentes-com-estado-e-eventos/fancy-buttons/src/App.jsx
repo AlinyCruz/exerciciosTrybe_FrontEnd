7339621a66c7f2ec4ed1ff293f7bca23fd44567d
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2022-11-30 01:15:58 -0300

    Seção03 - Dia 01 - Treinamento 03

--- 02-Front-End/Secao-03-Componentes-com-Estado-Eventos-e-Formularios-com-React/Dia-01-Componentes-com-estado-e-eventos/fancy-buttons/src/App.jsx
+++ 02-Front-End/Secao-03-Componentes-com-Estado-Eventos-e-Formularios-com-React/Dia-01-Componentes-com-estado-e-eventos/fancy-buttons/src/App.jsx
@@ -2,6 +2,7 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+
   eventoClick1() {
     console.log('Clicou no botão 1!');
   }
@@ -14,20 +15,25 @@ class App extends Component {
   
   // Assim:
 
-  // constructor() {
-  //   super()
-  //   // A função abaixo vincula "manualmente" o `this` à nossa função
-  //   this.eventoClick4 = this.eventoClick4.bind(this)
-  // }
-  // eventoClick4() {
-  //   console.log(this);
-  // }
+  constructor() {
+    super()
+    // A função abaixo vincula "manualmente" o `this` à nossa função
+    this.eventoClick4 = this.eventoClick4.bind(this)
+    this.state = {
+      numeroDeCliques: 0
+    }
+  }
+  eventoClick4() {
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques: estadoAnterior.numeroDeCliques + 1
+    }))
+  }
 
   // Ou assim, sem o 'bind' e com Arrow function:
   
-  eventoClick4 = () => {
-    console.log(this);
-  }
+  // eventoClick4 = () => {
+  //   console.log(this);
+  // }
 
   render() {
     return (
@@ -35,7 +41,7 @@ class App extends Component {
       <button onClick={this.eventoClick1}>Botão 01</button>
       <button onClick={this.eventoClick2}>Botão 02</button>
       <button onClick={this.eventoClick3}>Botão 03</button>
-      <button onClick={this.eventoClick4}>Botão 04</button>
+      <button onClick={this.eventoClick4}>{this.state.numeroDeCliques}</button>
       </>
     )
   }
