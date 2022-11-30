4b658b831019107c17fa8c49e1ac9676597ac24d
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2022-11-30 01:00:41 -0300

    Seção03 - Dia 01 - Ajustes

---
+++ 02-Front-End/Secao-03-Componentes-com-Estado-Eventos-e-Formularios-com-React/Dia-01-Componentes-com-estado-e-eventos/fancy-buttons/src/App.jsx
@@ -0,0 +1,44 @@
+// import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+class App extends Component {
+  eventoClick1() {
+    console.log('Clicou no botão 1!');
+  }
+  eventoClick2() {
+    console.log('Clicou no botão 2!!');
+  }
+  eventoClick3() {
+    console.log('Clicou no botão 3!!!');
+  }
+  
+  // Assim:
+
+  // constructor() {
+  //   super()
+  //   // A função abaixo vincula "manualmente" o `this` à nossa função
+  //   this.eventoClick4 = this.eventoClick4.bind(this)
+  // }
+  // eventoClick4() {
+  //   console.log(this);
+  // }
+
+  // Ou assim, sem o 'bind' e com Arrow function:
+  
+  eventoClick4 = () => {
+    console.log(this);
+  }
+
+  render() {
+    return (
+      <>
+      <button onClick={this.eventoClick1}>Botão 01</button>
+      <button onClick={this.eventoClick2}>Botão 02</button>
+      <button onClick={this.eventoClick3}>Botão 03</button>
+      <button onClick={this.eventoClick4}>Botão 04</button>
+      </>
+    )
+  }
+}
+
+export default App;
