f30adb90a6d7294cd0f6945e8144550cc98dd925
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2023-01-25 19:02:56 -0300

    Secao 06 - Dia 01 - Exercicio 01

---
+++ 02-Front-End/Seção-07-Gerenciamento-de-estado-com-Redux/Dia-01-Introdução-ao-Redux-O-estado-global-da-aplicação/script.js
@@ -0,0 +1,7 @@
+// Crie uma store para a nossa aplicação.
+
+const store = createStore(reducer);
+
+// Crie um reducer, implementando um switch que retorne apenas o estado inicial como default.
+
+const reducer = (state, action)
