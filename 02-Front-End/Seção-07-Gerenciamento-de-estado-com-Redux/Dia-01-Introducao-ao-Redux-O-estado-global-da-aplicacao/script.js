2374b8cf9ccf47a07608d916f7d865862b2852a5
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2023-01-26 13:14:30 -0300

    Secao 06 - Dia 01 - Exercicio 01 - feito com meu jeito

--- 02-Front-End/Seção-07-Gerenciamento-de-estado-com-Redux/Dia-01-Introducao-ao-Redux-O-estado-global-da-aplicacao/script.js
+++ 02-Front-End/Seção-07-Gerenciamento-de-estado-com-Redux/Dia-01-Introducao-ao-Redux-O-estado-global-da-aplicacao/script.js
@@ -1,12 +1,45 @@
-import { legacy_createStore as createStore } from "redux";
+const CHANGE_COLOR = 'CHANGE_COLOR';
 
-// Crie uma store para a nossa aplicação.
+// Criação da Action e do Dispatchs
+const actionChangeBtn = () => {
+  store.dispatch({ type: CHANGE_COLOR}) // <-- action
+}
 
-const store = createStore(reducer);
+// Captura dos botões
+const btnProx = document.querySelector('#previous');
+// const btnAnt = document.querySelector('#next');
 
-// Crie um reducer, implementando um switch que retorne apenas o estado inicial como default.
+// Evento nos botões
+btnProx.addEventListener('click', actionChangeBtn);
+// btnAnt.addEventListener('click', actionChange);
 
-const reducer = (state, action) => state
+// Estado inicial
+const INITIAL_STATE = {
+  colors: 'white',
+  // index: 0,
+};
+
+// Criação do redux
+const reducer = (state = INITIAL_STATE, action) => {
+  
+  switch (action.type) {
+    case CHANGE_COLOR:
+      const newColor = state.colors === 'white' ? 'black' : 'white';
+      return { colors: newColor }
+    default:
+      return state;
+    }
+  };
+
+// Criação do Store --- Não precisa mexer
+const store = Redux.createStore(reducer); // <-- recebe o reducer como parâmentro
+
+// Criação do Subscribe
+store.subscribe(() => {
+  const { colors } = store.getState();
+  document.querySelector('#value').innerHTML = colors;
+})
+
 
 
 
