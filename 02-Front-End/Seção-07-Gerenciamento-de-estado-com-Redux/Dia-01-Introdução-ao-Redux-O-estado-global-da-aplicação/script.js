48930740b6a465a809d1c3cd886815ae632ac40e
Aline Oliveira Cruz <108740455+AlinyCruz@users.noreply.github.com>
2023-01-26 13:16:12 -0300

    Delete 02-Front-End/Seção-07-Gerenciamento-de-estado-com-Redux/Dia-01-Introdução-ao-Redux-O-estado-global-da-aplicação directory

--- 02-Front-End/Seção-07-Gerenciamento-de-estado-com-Redux/Dia-01-Introdução-ao-Redux-O-estado-global-da-aplicação/script.js
+++
@@ -1,7 +0,0 @@
-// Crie uma store para a nossa aplicação.
-
-const store = createStore(reducer);
-
-// Crie um reducer, implementando um switch que retorne apenas o estado inicial como default.
-
-const reducer = (state, action)
